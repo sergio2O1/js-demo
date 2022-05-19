c0ebd1a1f5973bae59fe3d19e2f9849abc26a011
Sergey <s.kukartsev@white-soft.ru>
2022-05-19 20:03:24 +1000

    init commit

---
+++ script.js
@@ -0,0 +1,30 @@
+const BLOCKS_SLIDE_DELTA = 340;
+let blockShowedIndex = 0;
+
+window.onload = function() {
+    setInterval(
+        function() {
+            slideBlocks(true);
+        },
+        3000,
+    )
+}
+
+function slideBlocks(forward) {
+    const blockListElement = document.getElementById('block-list');
+    const blocksChildLength = blockListElement.children.length;
+    blockShowedIndex = forward
+        ? (blockShowedIndex + 1) % blocksChildLength
+        : !blockShowedIndex
+            ? blocksChildLength - 1
+            : blockShowedIndex - 1;
+
+    blockListElement.style.left = `${blockShowedIndex * (-BLOCKS_SLIDE_DELTA)}px`;
+}
+
+function addParagraph() {
+    const slider = document.getElementById('some-block');
+    const paragraphNode = document.createElement('p');
+    paragraphNode.innerText = 'Some paragraph added!';
+    slider.append(paragraphNode);
+}
